78005ea08e6006b2fa5f34e891e89994958d7c93
ezio1404 <zuming1404@gmail.com>
2018-02-25 05:54:44 +0800

    KUH ANA ANG EDIT !

--- controller/departmentController/updateDept.php
+++ controller/departmentController/updateDept.php
@@ -4,7 +4,7 @@ $department = new Department();
 if(isset($_POST['updatedept'])){
     $ref_id = $_POST['id'];
     $dept_code = $_POST['dept_code'];
-    $dept_description = $_POST['dept_code'];
+    $dept_description = $_POST['dept_description'];
     $office_location = $_POST['office_location'];
     // $ref_id=5;
     //     $dept_code = "BSHRM";
