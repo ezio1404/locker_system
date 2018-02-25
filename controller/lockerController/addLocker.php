ac8b8edf27590cf7fec2b7c9336b6e90311f5d78
ezio1404 <zuming1404@gmail.com>
2018-02-26 04:40:22 +0800

    DOne with locker

--- controller/lockerController/addLocker.php
+++ controller/lockerController/addLocker.php
@@ -4,13 +4,13 @@ $locker = new Locker();
 if(isset($_POST['addLocker'])){ // button name addLocker 
     
     $dept_id = $_POST['dept_id'];
-    $locker_status = $_POST['locker_status'];
+    $locker_status = "Vacant";
     // $dept_id = "2";
     // $locker_status = "Maintenance";
     for($i=0;$i<30;$i++){
-   $ok=$locker->addLocker(array($dept_id,$locker_status));
+  $locker->addLocker(array($dept_id,$locker_status));
 //    echo $ok?"Success":"Error";
     }
     
-    header('location:locker.php'); // redirect page
+    header('location:../../view/locker.html'); // redirect page
 }
