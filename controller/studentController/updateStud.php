9beeb6ccdbb89d1aabd2e045beff900123e836fe
ezio1404 <zuming1404@gmail.com>
2018-03-01 20:16:20 +0800

    Fixed student Update

--- controller/studentController/updateStud.php
+++ controller/studentController/updateStud.php
@@ -2,12 +2,11 @@
 require '../../model/studentModel.php';
 $student = new Student();
 if(isset($_POST['updateStud'])){
-    // $stud_id= $_POST['stud_id'];
-    $ref_id = $_POST['id'];
+    $stud_id= $_POST['id'];
     $stud_fname = $_POST['stud_fname'];
     $stud_lname = $_POST['stud_lname'];
-    $stud_yearLevel = $_POST['stud_yearLevel'];
     $dept_id=$_POST["dept_id"];
+    $stud_yearLevel = $_POST['stud_yearLevel'];
     $email = $_POST['email'];
     $date_apply = $_POST['date_apply'];
     // $stud_id = "15387467";
@@ -17,6 +16,6 @@ if(isset($_POST['updateStud'])){
     // $stud_yearLevel = "4";
     // $email = "ezio";
     // $date_apply = "1234";
-    $student->updateStud(array($stud_id, $stud_fname,$stud_lname,$dept_id, $stud_yearLevel,$email, $date_apply),$ref_id);
+    $student->updateStud(array($stud_id, $stud_fname,$stud_lname,$dept_id, $stud_yearLevel,$email, $date_apply),$stud_id);
     header('location:../../view/students.html');
 }
